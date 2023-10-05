2c446ff6fb8b9032455339f1409cc22f71d8a3af
lkaparos <146867275+lkaparos@users.noreply.github.com>
2023-10-04 22:55:49 -0700

    Session 1 prompt for coding journal 1

---
+++ codejournalone.py
@@ -0,0 +1,9 @@
+def print_lily():
+	print("Lily Kaparos")
+	print("she/her")
+	print("My favorite food is potatoes")
+	print("My favorite movie is Wall-e")
+def main():
+	print_lily()
+if __name__=="__main__":
+	main()
