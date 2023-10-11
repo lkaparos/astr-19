cc2b5c9bf49e0484111f577306d9a0733e6c29ab
lkaparos <146867275+lkaparos@users.noreply.github.com>
2023-10-11 13:45:09 -0700

    Add files via upload
    Session 2 prompt for Coding Journal 1

---
+++ codejournal2.py
@@ -0,0 +1,21 @@
+#sum of two floating point numbers
+x=3.5
+y=3.5
+sum=x+y 
+print(sum)
+print(type(sum))
+
+#difference between two integers
+x=10
+y=2
+x/y 
+int(x/y) 
+print(int(x/y))
+print(type(int(x/y)))
+
+#product of floating point number and integer
+x=5.0
+y=2
+x*y 
+print(x*y)
+print(type(x*y))
